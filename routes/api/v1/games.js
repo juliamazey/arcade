a2d4ebe8190051ed6e0ac2589eaddced7054c334
Julia Marco <marcootj@gmail.com>
2019-04-26 19:24:46 -0600

    added get single game

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 var Game = require('../../../models').Game;
 
+/* GET all games */
+
 router.get("/", function(req, res, next) {
   Game.findAll()
     .then(games => {
@@ -13,5 +15,24 @@ router.get("/", function(req, res, next) {
       res.status(500).send({error})
     });
 });
+
+/* GET single game */
+
+router.get("/:id", function(req, res, next) {
+  Game.findAll({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      rest.status(500).send({ error })
+    });
+});
+
 
 module.exports = router;
